af1b931bc8596f00e15dbb82acb9c4c25e34c0f9
matthewvadeboncoeur <151592935+matthewvadeboncoeur@users.noreply.github.com>
2025-08-03 02:16:58 -0700

    implemented input and button for sending messages

--- script.js
+++ script.js
@@ -2,19 +2,18 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const addFriendButton = document.getElementById('add-friend-button');
-    const addFriendInput = document.getElementById('add-friend-input');
-
     addFriendButton.addEventListener('click', addFriend);
 
 
     function addFriend() {
         const addFriendInput = document.getElementById('add-friend-input');
+        if (addFriendInput.value == '')
+            return;
         const friendsList = document.getElementById('friends-list');
         const newFriendLi = document.createElement('li');
         newFriendLi.textContent = addFriendInput.value;
         friendsList.appendChild(newFriendLi);
         addFriendInput.value = '';
-
     }
 
 
